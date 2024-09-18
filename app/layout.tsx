52c5136711c1e65299aaf71751708d13dfb66763
Bennet-Ukoh <bennetukoh@gmail.com>
2024-09-18 12:37:19 +0100

    minor correction in intro and metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,7 +11,7 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Bennet | Personal Portfolio",
-  description: "Bennet is a full-stack developer with 2 years of experience.",
+  description: "Bennet is a Frontend developer with 2+ years of experience.",
 };
 
 export default function RootLayout({
