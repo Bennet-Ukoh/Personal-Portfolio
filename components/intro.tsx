19a48ce3f92515067f00a424f31991e65d6bae35
Bennet-Ukoh <bennetukoh@gmail.com>
2025-04-03 15:55:35 +0100

    Update intro.tsx

--- components/intro.tsx
+++ components/intro.tsx
@@ -64,9 +64,8 @@ export default function Intro() {
         animate={{ opacity: 1, y: 0 }}
       >
         <span className="font-bold">Hello, I'm Bennet.</span> I'm a{" "}
-        <span className="font-bold">Frontend developer</span> with{" "}
-        <span className="font-bold">2+ years</span> of experience. I enjoy
-        building <span className="italic">sites & apps</span>. My focus is{" "}
+        <span className="font-bold">Frontend developer</span> that{" "}
+        <span className="font-bold">enjoy building responsive sites and apps.</span> My focus is{" "}
         <span className="underline">React (Next.js)</span>.
       </motion.h1>
 
