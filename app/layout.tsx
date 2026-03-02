b285f567c6b654a0300dcae035d4dd045bdffb5b
Bennet-Ukoh <bennetukoh@gmail.com>
2026-03-02 09:40:13 +0000

    refactor: move layout components to layout directory

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,8 @@
-import Header from "@/components/header";
+import Header from "@/components/layout/header";
 import "./globals.css";
 import { Inter } from "next/font/google";
 import ActiveSectionContextProvider from "@/context/active-section-context";
-import Footer from "@/components/footer";
+import Footer from "@/components/layout/footer";
 import ThemeSwitch from "@/components/theme-switch";
 import ThemeContextProvider from "@/context/theme-context";
 import { Toaster } from "react-hot-toast";
