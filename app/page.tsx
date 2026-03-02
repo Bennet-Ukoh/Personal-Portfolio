59267e65c3d7626ab9dca7ac990e5f86257286b1
Bennet-Ukoh <bennetukoh@gmail.com>
2026-03-02 10:03:03 +0000

    refactor: file structure well organized

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
 import Intro from "@/features/home/components/intro";
 import SectionDivider from "@/components/ui/section-divider";
-import About from "@/components/about";
+import About from "@/features/home/components/about";
 import Projects from "@/features/projects/components/projects";
-import Skills from "@/components/skills";
-import Experience from "@/components/experience";
+import Skills from "@/features/home/components/skills";
+import Experience from "@/features/home/components/experience";
 import Contact from "@/features/contact/components/contact";
 
 export default function Home() {
